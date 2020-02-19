fbc0808bba00efc1550703f51cc1e610be804815
jacekpozo@gmail.com <Pozowski94>
2020-02-19 09:12:48 +0100

    fix db

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var flash=require('connect-flash');
 var app = express();
 var user= require('./models/user');
 var judge= require('./models/judge');
-mongoose.connect('mongodb+svr://Pozo:Meliana1@cluster0-zrsck.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true});
+mongoose.connect('mongodb+svr://Pozo:Meliana1@cluster0-zrsck.mongodb.net/test?retryWrites=true&w=majority/Zawody',{useNewUrlParser: true});
 var db = mongoose.connection;
 require('./config/prod')(app);
 
