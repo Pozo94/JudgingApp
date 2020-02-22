30971b64716fdebeceedfac9488e381281dccbc8
jacekpozo@gmail.com <Pozowski94>
2020-02-22 10:19:42 +0100

    Final changes

--- routes/results.js
+++ routes/results.js
@@ -16,126 +16,170 @@ router.get('/', function (req, res) {
 router.get('/league/I',function (req,res) {
 
 
-})
+});
 router.get('/league/II',function (req,res) {
 
 
-})
+});
 router.get('/league/III',function (req,res) {
 
 
-})
+});
 router.get('/league/mlodziezowa',function (req,res) {
 
 
-})
+});
 router.get('/league/sum',function (req,res) {
 
     Current_Edition.db.collection('i_editions').find({clas:'Młodzieżowa'}).sort(method).toArray( function(err, competitors){
-        for(i=0;i<15;i++){
-            if (i===0) competitors[i].score=competitors[i].score+75;
-            if (i===1) competitors[i].score=competitors[i].score+60;
-            if (i===2) competitors[i].score=competitors[i].score+45;
-            if (i===3) competitors[i].score=competitors[i].score+35;
-            if (i===4) competitors[i].score=competitors[i].score+30;
-            if (i===5) competitors[i].score=competitors[i].score+25;
-            if (i===6) competitors[i].score=competitors[i].score+20;
-            if (i===7) competitors[i].score=competitors[i].score+15;
-            if (i===8) competitors[i].score=competitors[i].score+12;
-            if (i===9) competitors[i].score=competitors[i].score+10;
-            if (i===10) competitors[i].score=competitors[i].score+9;
-            if (i===11) competitors[i].score=competitors[i].score+8;
-            if (i===12) competitors[i].score=competitors[i].score+7;
-            if (i===13) competitors[i].score=competitors[i].score+6;
-            if (i===14) competitors[i].score=competitors[i].score+5;
-            League.update({competitor:competitors[i].competitor},competitors[i],function (err) {
-                if (err){
-                    console.log(err)
-                    return
-                }
+
+        for(let i=0;i<Object.keys(competitors).length;i++)
+        {   console.log(i)
+            console.log(competitors[i])
+            League.findOne({competitor:competitors[i].competitor},function (err,league) {
+                //console.log(i)
+                if (i===0){league.score=league.score+75;}
+                if (i===1){league.score=league.score+60;}
+                if (i===2){league.score=league.score+45;}
+                if (i===3){league.score=league.score+35;}
+                if (i===4){league.score=league.score+30;}
+                if (i===5){league.score=league.score+25;}
+                if (i===6){league.score=league.score+20;}
+                if (i===7){league.score=league.score+15;}
+                if (i===8){league.score=league.score+12;}
+                if (i===9){league.score=league.score+10;}
+                if (i===10){league.score=league.score+9;}
+                if (i===11){league.score=league.score+8;}
+                if (i===12){league.score=league.score+7;}
+                if (i===13){league.score=league.score+6;}
+                if (i===14){league.score=league.score+5;}
+
+                League.update({competitor:league.competitor},league,function (err) {
+                    if (err){
+                        console.log(err)
+                        return
+                    }
+                    else{
+                        console.log()
+                    }
+
+                })
 
             })
+
 
         }
-    })
+    });
     Current_Edition.db.collection('i_editions').find({clas:'III'}).sort(method).toArray( function(err, competitors){
-        for(i=0;i<15;i++){
-            if (i===0) competitors[i].score=competitors[i].score+130;
-            if (i===1) competitors[i].score=competitors[i].score+115;
-            if (i===2) competitors[i].score=competitors[i].score+100;
-            if (i===3) competitors[i].score=competitors[i].score+85;
-            if (i===4) competitors[i].score=competitors[i].score+75;
-            if (i===5) competitors[i].score=competitors[i].score+65;
-            if (i===6) competitors[i].score=competitors[i].score+55;
-            if (i===7) competitors[i].score=competitors[i].score+45;
-            if (i===8) competitors[i].score=competitors[i].score+35;
-            if (i===9) competitors[i].score=competitors[i].score+25;
-            if (i===10) competitors[i].score=competitors[i].score+20;
-            if (i===11) competitors[i].score=competitors[i].score+15;
-            if (i===12) competitors[i].score=competitors[i].score+12;
-            if (i===13) competitors[i].score=competitors[i].score+10;
-            if (i===14) competitors[i].score=competitors[i].score+8;
-            League.update({competitor:competitors[i].competitor},competitors[i],function (err) {
-                if (err){
-                    console.log(err)
-                    return
-                }
+        for(let i=0;i<Object.keys(competitors).length;i++)
+        {   console.log(i)
+            console.log(competitors[i])
+            League.findOne({competitor:competitors[i].competitor},function (err,league) {
+                //console.log(i)
+                if (i===0){league.score=league.score+130;}
+                if (i===1){league.score=league.score+115;}
+                if (i===2){league.score=league.score+100;}
+                if (i===3){league.score=league.score+85;}
+                if (i===4){league.score=league.score+75;}
+                if (i===5){league.score=league.score+65;}
+                if (i===6){league.score=league.score+55;}
+                if (i===7){league.score=league.score+45;}
+                if (i===8){league.score=league.score+35;}
+                if (i===9){league.score=league.score+25;}
+                if (i===10){league.score=league.score+20;}
+                if (i===11){league.score=league.score+15;}
+                if (i===12){league.score=league.score+12;}
+                if (i===13){league.score=league.score+10;}
+                if (i===14){league.score=league.score+8;}
+
+                League.update({competitor:league.competitor},league,function (err) {
+                    if (err){
+                        console.log(err)
+                        return
+                    }
+                    else{
+                        console.log()
+                    }
+
+                })
 
             })
+
 
         }
     })
     Current_Edition.db.collection('i_editions').find({clas:'II'}).sort(method).toArray( function(err, competitors){
-        for(i=0;i<15;i++){
-            if (i===0) competitors[i].score=competitors[i].score+200;
-            if (i===1) competitors[i].score=competitors[i].score+180;
-            if (i===2) competitors[i].score=competitors[i].score+160;
-            if (i===3) competitors[i].score=competitors[i].score+145;
-            if (i===4) competitors[i].score=competitors[i].score+130;
-            if (i===5) competitors[i].score=competitors[i].score+115;
-            if (i===6) competitors[i].score=competitors[i].score+100;
-            if (i===7) competitors[i].score=competitors[i].score+80;
-            if (i===8) competitors[i].score=competitors[i].score+70;
-            if (i===9) competitors[i].score=competitors[i].score+60;
-            if (i===10) competitors[i].score=competitors[i].score+55;
-            if (i===11) competitors[i].score=competitors[i].score+50;
-            if (i===12) competitors[i].score=competitors[i].score+40;
-            if (i===13) competitors[i].score=competitors[i].score+35;
-            if (i===14) competitors[i].score=competitors[i].score+30;
-            League.update({competitor:competitors[i].competitor},competitors[i],function (err) {
-                if (err){
-                    console.log(err)
-                    return
-                }
+        for(let i=0;i<Object.keys(competitors).length;i++)
+        {
+            League.findOne({competitor:competitors[i].competitor},function (err,league) {
+                //console.log(i)
+                if (i===0){league.score=league.score+200;}
+                if (i===1){league.score=league.score+180;}
+                if (i===2){league.score=league.score+160;}
+                if (i===3){league.score=league.score+145;}
+                if (i===4){league.score=league.score+130;}
+                if (i===5){league.score=league.score+115;}
+                if (i===6){league.score=league.score+100;}
+                if (i===7){league.score=league.score+80;}
+                if (i===8){league.score=league.score+70;}
+                if (i===9){league.score=league.score+60;}
+                if (i===10){league.score=league.score+55;}
+                if (i===11){league.score=league.score+50;}
+                if (i===12){league.score=league.score+40;}
+                if (i===13){league.score=league.score+35;}
+                if (i===14){league.score=league.score+30;}
+
+                League.update({competitor:league.competitor},league,function (err) {
+                    if (err){
+                        console.log(err)
+                        return
+                    }
+                    else{
+                        console.log()
+                    }
+
+                })
 
             })
+
 
         }
-    })
+    });
     Current_Edition.db.collection('i_editions').find({clas:'I'}).sort(method).toArray( function(err, competitors){
-        for(i=0;i<15;i++){
-            if (i===0) competitors[i].score=competitors[i].score+300;
-            if (i===1) competitors[i].score=competitors[i].score+250;
-            if (i===2) competitors[i].score=competitors[i].score+200;
-            if (i===3) competitors[i].score=competitors[i].score+175;
-            if (i===4) competitors[i].score=competitors[i].score+150;
-            if (i===5) competitors[i].score=competitors[i].score+130;
-            if (i===6) competitors[i].score=competitors[i].score+110;
-            if (i===7) competitors[i].score=competitors[i].score+90;
-            if (i===8) competitors[i].score=competitors[i].score+80;
-            if (i===9) competitors[i].score=competitors[i].score+75;
-            if (i===10) competitors[i].score=competitors[i].score+70;
-            if (i===11) competitors[i].score=competitors[i].score+65;
-            if (i===12) competitors[i].score=competitors[i].score+60;
-            if (i===13) competitors[i].score=competitors[i].score+55;
-            if (i===14) competitors[i].score=competitors[i].score+50;
-            League.update({competitor:competitors[i].competitor},competitors[i],function (err) {
-                if (err){
-                    console.log(err)
-                    return
-                }
+        for(let i=0;i<Object.keys(competitors).length;i++)
+        {   console.log(i)
+            console.log(competitors[i])
+            League.findOne({competitor:competitors[i].competitor},function (err,league) {
+                //console.log(i)
+                if (i===0){league.score=league.score+300;}
+                if (i===1){league.score=league.score+250;}
+                if (i===2){league.score=league.score+200;}
+                if (i===3){league.score=league.score+175;}
+                if (i===4){league.score=league.score+150;}
+                if (i===5){league.score=league.score+130;}
+                if (i===6){league.score=league.score+110;}
+                if (i===7){league.score=league.score+90;}
+                if (i===8){league.score=league.score+80;}
+                if (i===9){league.score=league.score+75;}
+                if (i===10){league.score=league.score+70;}
+                if (i===11){league.score=league.score+65;}
+                if (i===12){league.score=league.score+60;}
+                if (i===13){league.score=league.score+55;}
+                if (i===14){league.score=league.score+50;}
+
+                League.update({competitor:league.competitor},league,function (err) {
+                    if (err){
+                        console.log(err)
+                        return
+                    }
+                    else{
+                        console.log()
+                    }
+
+                })
 
             })
+
 
         }
     })
@@ -145,7 +189,7 @@ router.get('/league/sum',function (req,res) {
 
 router.get('/mistrzowska', function (req, res) {
 
-    Current_Edition.db.collection('i_editions').find().sort(method).toArray( function (err,competitors) {
+    Current_Edition.db.collection('i_editions').find({clas:"Mistrzowska"}).sort(method).toArray( function (err,competitors) {
 
 
 
